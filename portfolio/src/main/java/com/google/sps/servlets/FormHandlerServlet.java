2cefc16c1671aedc115621e0ba94dc2bb287aca3
Shaohua Chen <shirleyc815@berkeley.edu>
2022-06-24 20:37:37 +0000

    week4 complete

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,5 +1,14 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +22,19 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String textValue = Jsoup.clean(request.getParameter("text-input"), Whitelist.none());
+
+    // Get current time
+    long timestamp = System.currentTimeMillis();
+
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Input");
+    FullEntity inputEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("userInput", textValue)
+            .set("timestamp", timestamp)
+            .build();
+    datastore.put(inputEntity);
 
     // Print the value so you can see it in the server logs.
     System.out.println("You submitted: " + textValue);
